7b718298713988456ff6ede0df774d551321ee77
JimCampagno <jimcampagno@gmail.com>
2017-03-21 09:28:34 -0400

    SparseArray start

---
+++ arrays/5.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: ![](SparseArrays-1.png)
+//: ![](SparseArrays-2.png)
+let n = 4
+let string1 = "aba"
+let string2 = "baba"
+let string3 = "aba"
+let string4 = "xzxb"
+
+let otherN = 3
+
+
+
+
+
+
